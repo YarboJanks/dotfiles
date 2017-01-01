9e41b08a6064f12b405e50011a954deed0f906b5
Alex Flores <domain@me.com>
2016-12-31 20:42:23 -0500

    fix gpg socket stuff

--- .zshrc
+++ .zshrc
@@ -33,9 +33,8 @@ alias reload="source ~/.zshrc"
 
 # Temp aliases
 alias kedit="cd $HOME/Code/keyboards/atreus-firmware/atreus/; e keymap_alex.c"
-alias pressh="gpg-connect-agent updatestartuptty /bye"
 
 # necessary when using Termite
-[ "$TERM" = "xterm-termite" ] && alias ssh="pressh && TERM=screen-256color ssh"
+[ "$TERM" = "xterm-termite" ] && alias ssh="TERM=screen-256color ssh"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- zsh-files/plugins/gpg/gpg.plugin.zsh
+++ zsh-files/plugins/gpg/gpg.plugin.zsh
@@ -1,16 +1,15 @@
 # Set SSH to use gpg-agent
 unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
-fi
+export SSH_AUTH_SOCK=/run/user/1000/gnupg/S.gpg-agent.ssh
 
 # Start the gpg-agent if not already running
 if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
+  echo 'starting gpg agent for the first time'
   eval $(gpg-agent --daemon --options ~/.gnupg/gpg-agent.conf)
-  # gpg-connect-agent /bye >/dev/null 2>&1
 fi
 
 # Set GPG TTY
 export GPG_TTY=$(tty)
+
 # Refresh gpg-agent tty in case user switches into an X session
 gpg-connect-agent updatestartuptty /bye >/dev/null
